88bea71bf63fa3bb34deeba1fa4358d5047e8917
ApanasenkoOleg <apanasenkooleg10000@gmail.com>
2025-01-19 14:35:08 +0300

    Update correction_db.sql

--- ex2/correction_db.sql
+++ ex2/correction_db.sql
@@ -1,17 +1,27 @@
--- Удаление пустых категорий (групп), в которых нет товаров
-DELETE FROM `categories`
-WHERE `id` NOT IN (
-    SELECT DISTINCT `category_id` FROM `products`
-);
-
 -- Удаление товаров, которые не имеют наличия на складах
-DELETE FROM `products`
-WHERE `id` NOT IN (
-    SELECT DISTINCT `product_id` FROM `availabilities`
+DELETE FROM products
+WHERE id NOT IN (
+    SELECT DISTINCT product_id FROM availabilities
 );
 
 -- Удаление складов, на которых нет товаров
-DELETE FROM `stocks`
-WHERE `id` NOT IN (
-    SELECT DISTINCT `stock_id` FROM `availabilities`
+DELETE FROM stocks
+WHERE id NOT IN (
+    SELECT DISTINCT stock_id FROM availabilities
 );
+
+-- Удаление записей из availabilities с несуществующими product_id или stock_id
+DELETE FROM availabilities
+WHERE product_id NOT IN (
+    SELECT id FROM products
+)
+OR stock_id NOT IN (
+    SELECT id FROM stocks
+);
+
+-- Удаление пустых категорий (групп), в которых нет товаров
+DELETE FROM categories
+WHERE id NOT IN (
+    SELECT DISTINCT category_id FROM products
+);
+
